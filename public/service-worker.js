4e6769364d61046bfc8f593b41cfef9f3927ecdc
David A. Lindner Jr <65383133+davidlindnerjr@users.noreply.github.com>
2020-10-28 20:15:29 -0700

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,7 +7,9 @@ const FILES_TO_CACHE = [
     "/index.html",
     "index.js", 
     "/db.js",
-    "/styles.css"
+    "/styles.css",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png"
 ];
 
 self.addEventListener("install", function(evt) {
